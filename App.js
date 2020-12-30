8f7a48fbfa412fe92e11897f1f832eae52d1bbee
nmakhari <nmakhari@uwaterloo.ca>
2020-12-29 21:07:45 -0500

    header created

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header.js';
 
 export default function App() {
+
+  const [tasks, setTasks] = useState([
+    {text: 'test task', key: '1'}
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <View> 
+        {/* input here */}
+      </View>
+
+      <View>
+        {/* list here */}
+      </View>
+
     </View>
   );
 }
@@ -14,8 +26,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: 'white'
+  }
 });
---
+++ components/header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default header = () => {
+    
+    return (
+        <View style={style.header}>
+            <Text style={style.headerText}>Todo List</Text>
+        </View>
+    );
+
+}
+
+const style = StyleSheet.create({
+    header: {
+        backgroundColor: 'coral',
+        alignItems: 'center'
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        paddingTop: 40,
+        padding: 20,
+        fontFamily: 'Roboto' // necessary for one plus and other OS which cannot handle the bold text correctly
+    }
+});
