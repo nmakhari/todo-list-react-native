40be4b69b62031edf5a0659234f85e926533b698
nmakhari <nmakhari@uwaterloo.ca>
2020-12-30 13:22:38 -0500

    added delete icon to each list task

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default ListItem = ({task, completionHandler}) => {
     return (
         <TouchableOpacity
             onPress={() => completionHandler(task.key)}>
             <View style={styles.listItem}>
+                <MaterialIcons name="delete" size={24} color="black" style={styles.icon}/>
                 <Text style={styles.itemText}>{task.text}</Text>
             </View>
         </TouchableOpacity>
@@ -15,6 +17,9 @@ export default ListItem = ({task, completionHandler}) => {
 
 const styles = StyleSheet.create({
     listItem: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
         borderStyle: 'dashed',
         borderColor: 'grey',
         borderWidth: 1,
@@ -25,8 +30,12 @@ const styles = StyleSheet.create({
         borderRadius: 10
     },
     itemText: {
+        flex: 5,
         fontSize: 15,
         fontFamily: 'Roboto',
         fontWeight: 'bold'
+    },
+    icon: {
+        flex: 1,
     }
 });
